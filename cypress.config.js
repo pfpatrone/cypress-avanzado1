258d5ef995c3da362b77ff66c9026f7ad84c10e2
Pablo Patrone <pablofpatrone@gmail.com>
2024-03-06 10:54:15 -0300

    desafio1-final

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      specPattern: "cypress/e2e/**/*.js"
+    },
+
+    baseUrl: "https://pushing-it.vercel.app",
+    fixturesFolder: "cypress/e2e/",
+    defaultCommandTimeout: 5000
+  },
+});
